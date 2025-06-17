641e130e76f0736945b6ad23c40e5842230dac55
GianniC88 <gianni.colucci.88@gmail.com>
2025-06-17 18:11:58 +0200

    Riorganizza il codice per la richiesta AJAX e aggiungi il ciclo per elaborare i dati ricevuti dall'API

--- main.js
+++ main.js
@@ -9,33 +9,31 @@ console.log(rowEl);
 //creare una variabile per contenere l'endpoint dell'API REST da chiamare
 const endpointUrl = "https://lanciweb.github.io/demo/api/pictures/";
 
-//eseguire una richiesta ajax all'endpoint per ottenere i dati
+//eseguire una richiesta ajax all'endpoint per ottenere i dati API che restituisce dati in formato JSON in JavaScript.
 fetch(endpointUrl)
   .then((res) => res.json())
   .then((data) => {
     console.log(data);
+
+    //-eseguire un ciclo sui dati per ottenere il singolo oggetto post
+    data.forEach((post) => {
+      //-creare il markup con il template literal da utilizzare per la card della colonna dei singoli post
+      //valori dall'oggetto destrutturato da inserire
+      const { title, date, url } = post;
+      console.log(post)
+      const postMarkupStr = `     
+        <div class="col-4">
+          <div class="card">
+            <img class="pin" src="./img/pin.svg" alt="" />
+            <img class="padding" src="img/img card.jpeg" class="card-img-top" alt="..." />
+            <div class="card-body">
+              <p class="card-text">Accusamus Beate ad Facilis Cum Similique Qui Sunt</p>
+            </div>
+          </div>
+         </div>`;
+
+
+    });
+
+    //aggiorno gli elementi della DOM (roeEl)
   });
-
-//-eseguire un ciclo sui dati per ottenere il singolo oggetto post
-
-//-creare il markup con il template literal da utilizzare per la card della colonna dei singoli post
-
-//valori dall'oggetto destrutturato da inserire
-/*
-<div class="col-4">
-  <div class="card">
-    <img class="pin" src="./img/pin.svg" alt="" />
-    <img
-      class="padding"
-      src="img/img card.jpeg"
-      class="card-img-top"
-      alt="..."
-    />
-    <div class="card-body">
-      <p class="card-text">Accusamus Beate ad Facilis Cum Similique Qui Sunt</p>
-    </div>
-  </div>
-</div>;
- xs*/
-
-//aggiorno gli elementi della DOM (roeEl)
